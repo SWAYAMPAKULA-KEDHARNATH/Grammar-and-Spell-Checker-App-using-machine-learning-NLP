900022c2dbdb43ed2199fd2bee055ee6531b55b8
SWAYAMPAKULA-KEDHARNATH <91668365+SWAYAMPAKULA-KEDHARNATH@users.noreply.github.com>
2023-10-29 14:49:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,44 @@
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template
 from Model import SpellCheckerModule
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Set a secret key for the session
+
 spell_checker_module = SpellCheckerModule()
 
 # routes
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/spell',methods=['POST','GET'])
+
+@app.route('/spell', methods=['POST', 'GET'])
 def spell():
-    if request.method=='POST':
+    corrected_text = ""
+    corrected_grammar = []
+    
+    if request.method == 'POST':
         text = request.form['text']
         corrected_text = spell_checker_module.correct_spell(text)
-        corrected_grammar = spell_checker_module.correct_grammar(text)
-        return render_template('index.html',corrected_text=corrected_text,corrected_grammar=corrected_grammar)
-@app.route('/grammar',methods=['POST','GET'])
+        _, corrected_grammar = spell_checker_module.correct_grammar(text)
+        
+    return render_template('index.html', corrected_text=corrected_text, corrected_grammar=corrected_grammar)
+
+@app.route('/grammar', methods=['POST', 'GET'])
 def grammar():
+    corrected_file_text = ""
+    corrected_file_grammar = []
+    
     if request.method == 'POST':
         file = request.files['file']
-        readable_file = file.read().decode('utf-8',errors='ignore')
-        corrected_file_text = spell_checker_module.correct_spell(readable_file)
-        corrected_file_grammar = spell_checker_module.correct_grammar(readable_file)
-    return render_template('index.html',corrected_file_text=corrected_file_text,corrected_file_grammar=corrected_file_grammar)
+        try:
+            readable_file = file.read().decode('utf-8', errors='ignore')
+            corrected_file_text = spell_checker_module.correct_spell(readable_file)
+            _, corrected_file_grammar = spell_checker_module.correct_grammar(readable_file)
+        except Exception as e:
+            # Handle the error, for example, print it for debugging purposes
+            print("Error occurred:", e)
+    
+    return render_template('index.html', corrected_file_text=corrected_file_text, corrected_file_grammar=corrected_file_grammar)
 
 # python main
 if __name__ == "__main__":
